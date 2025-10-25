0e3b1f6a7dd23f72762b73fa0832bc52783c17c9
azertyIA <twinarmageddons11011@gmail.com>
2025-10-24 23:27:09 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# blackjack-nn
+
+Just a neural network.
+
+## Building
+
+Get cmake.
+
+```
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
---
+++ tests/test_mat.cpp
@@ -0,0 +1,28 @@
+#include "mat.h"
+#include <cassert>
+#include <cstdio>
+
+bool test_matrix_fill() { return true; }
+
+bool test_matrix_add() { return true; }
+
+bool test_matrix_mult() { return true; }
+
+int main() {
+  struct {
+    const char *name;
+    bool (*fn)();
+  } tests[] = {
+      {"matrix_fill", test_matrix_fill},
+      {"matrix_add", test_matrix_add},
+      {"matrix_mult", test_matrix_mult},
+  };
+
+  int failures = 0;
+  for (auto &t : tests) {
+    bool ok = t.fn();
+    if (!ok)
+      failures++;
+  }
+  return failures ? 1 : 0;
+}
